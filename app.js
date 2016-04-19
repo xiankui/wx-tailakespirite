c45acddb319d8d90e66834c6509db80af03a66af
研发中心-朱献奎15045 <zxk15045@tcent.cn>
2016-04-19 17:08:10 +0800

    update

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ app.post('/', xmlparser({trim: false, explicitArray: false}), function (req, res
 		data = '我好，你也好！';		
 	} 
 
+	var xml = '';
+	xml += '<xml>';
+	xml += '<ToUserName>gh_965ad0d8c4c4</ToUserName>';
+	xml += '<FromUserName>oQAJ_wdJQrm_IuHXLVnr2RVVSNGY</FromUserName>';
+	xml += '<CreateTime>12345678</CreateTime>';
+	xml += '<MsgType>text</MsgType>';
+	xml += '<Content>'+ data +'</Content>';
+	xml += '</xml>';
+
+	res.writeHead(200, {'Content-Type': 'application/xml'});
 	res.send(data);	
 })
 
@@ -46,17 +56,28 @@ app.get('/getaccesstoken', function (req, res) {
 
 app.get('/getiplist', function (req, res) {
 
-	var option = app.get('/getaccesstoken');
-	console.log(option)
+	var option = {
+		appid: appid,
+		secret: secret
+	};
 
-	wxInterface.getCallBackIP(option, function (err, iplist) {
+	wxInterface.getAccessToken(option, function (err, access_token) {
 		if (err) {
-			console.log('getCallBackIP error');
+			console.error('getAccessToken error');
 			return;
 		}
 
-		res.send(iplist);
-	})
+		wxInterface.getCallBackIP(access_token, function (err, iplist) {
+			if (err) {
+				console.log('getCallBackIP error');
+				return;
+			}
+
+			res.send(iplist);
+		})
+	});
+
+	
 })
 
 app.listen(80, '120.26.38.84');
